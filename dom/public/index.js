6e8b6b8bc26b84ff33d607b1cbd0b4d2f3bcd4e3
Joaquin <joaco.aravena02@gmail.com>
2024-03-30 12:23:36 -0300

    Refactor en index.html, parte DOM terminada

--- dom/public/index.js
+++
@@ -1,5 +0,0 @@
-
-let arqueros = ["12. Leandro Brey"]
-let defensores = ["42. Lucas Blondel", "38. Aaron Anselmino", "6. Marcos Rojo","23. Lautaro Blanco"]
-let mediocampistas = ["21. Equi Fernandez", "47. Jabes Saralegui", "22. Kevin Zenón", "39. Vicente Taborda"]
-let delanteros = ["16. Miguel Merentiel", "10. Edinson Cavani"]
